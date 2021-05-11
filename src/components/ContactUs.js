4418b3969597b89168f741500ce3bc668ab646b0
Linus <limpan.ml@hotmail.com>
2021-05-11 15:05:41 +0200

    Påbörjat design av kontaktsidan

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './ContactUS.css'
 
 function ContactUs() {
     return (
-        <div>
-            <h1>Contact us sida</h1>
+        <div class="contactpage-content">
+            <h1>Kontaktsida </h1>
+            <div id="teaminfosection">
+                <h2> Vårt team </h2>
+            </div>
+            <div id="employeeinfo">
+                <h3> Anställda </h3>
+            </div>
         </div>
     )
 }
