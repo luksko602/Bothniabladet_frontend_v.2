1faaec14f34999d22b7cd1837e19402e106443ae
Linus <limpan.ml@hotmail.com>
2021-05-11 15:59:15 +0200

    Fortsatt med design av kontaktsidan

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,10 +7,24 @@ function ContactUs() {
             <h1>Kontaktsida </h1>
             <div id="teaminfosection">
                 <h2> Vårt team </h2>
+                <p> Här ska info om de anställda stå </p>
+                <div id="employeeinfo">
+                <h2> Anställda </h2>
+                <div id="editorinfo">
+                    <h3> Redaktör </h3>
+                </div>
+                <div id="photographerinfo">
+                    <h3> Fotograf </h3>
+                </div>
+                <div id="contenteditor">
+                    <h3> Redigerare </h3>
+                </div>
+                <div id="archivesinfo">
+                    <h3> Arkiv </h3>
+                </div>
             </div>
-            <div id="employeeinfo">
-                <h3> Anställda </h3>
             </div>
+           
         </div>
     )
 }
