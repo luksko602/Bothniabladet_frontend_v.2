670b805eafd5b83cac6e56caf1025871cd18d752
Linus <limpan.ml@hotmail.com>
2021-05-16 13:11:58 +0200

    Skapade filer för en footer på applikationen, fortsatte med design av kontaktsidan

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+    return(
+        <div className="footer">
+            <h1> Detta är applikationens footer </h1>
+        </div>
+    );
+}
