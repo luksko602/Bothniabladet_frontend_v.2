d92c66a62ff3bf341d809c1bc0bfff0ac2e47408
luksko602 <65337371+luksko602@users.noreply.github.com>
2021-05-21 14:03:57 +0200

    bug fixes

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,24 +1,22 @@
 export const MenuItems = [
-    {
-        title: 'Hem',
-        path: '/',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Ladda upp',
-        path: '/upload',
-        cName: 'nav-links'
-    }, 
-    {
-        title: 'Kontakta oss',
-        path: '/contact-us',
-        cName: 'nav-links'
-    },  
-    {
-        title: 'Logga in',
-        path: '/signin',
-        cName: 'nav-links-mobile'
-    },   
-
-   
-]
+  {
+    title: "Hem",
+    path: "/",
+    cName: "nav-links",
+  },
+  {
+    title: "Ladda upp",
+    path: "/upload",
+    cName: "nav-links",
+  },
+  {
+    title: "Kontakta oss",
+    path: "/contact-us",
+    cName: "nav-links",
+  },
+  {
+    title: "Mitt konto",
+    path: "/account",
+    cName: "nav-links-mobile",
+  },
+];
